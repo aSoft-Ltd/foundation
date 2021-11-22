2fac9860464baa09d471319484051425334ba993
andylamax <andylamax@programmer.net>
2021-11-23 01:11:20 +0300

    Testing the nexus publish plugin

--- foundation-runtimes/build.gradle.kts
+++ foundation-runtimes/build.gradle.kts
@@ -3,4 +3,20 @@ plugins {
     alias(plugs.plugins.kotlin.multiplatform) apply false
     alias(plugs.plugins.kotlin.serialization) apply false
     alias(plugs.plugins.nexus.staging) apply false
+    alias(plugs.plugins.nexus.publish)
+}
+
+
+afterEvaluate {
+    group = "tz.co.asoft"
+    version = asoft.versions.foundation
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("ASOFT_NEXUS_USERNAME"))
+            password.set(System.getenv("ASOFT_NEXUS_PASSWORD"))
+        }
+    }
 }
